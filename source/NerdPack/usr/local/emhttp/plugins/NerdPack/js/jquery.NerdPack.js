ce943946998209743e40d56dcbe36210bd4bd51a
Derek Macias <derekmacias@live.com>
2016-01-25 00:01:51 -0700

    rename to reset filters

--- source/NerdPack/usr/local/emhttp/plugins/NerdPack/js/jquery.NerdPack.js
+++ source/NerdPack/usr/local/emhttp/plugins/NerdPack/js/jquery.NerdPack.js
@@ -1,22 +1,4 @@
 $(function(){
-	// tablesorter options
-	$("#tblPackages").tablesorter({
-		sortList: [[0,1]],
-		widgets: ["saveSort", "filter", "stickyHeaders"],
-		widgetOptions: {
-			stickyHeaders_filteredToTop: true,
-			filter_hideEmpty: true,
-			filter_liveSearch: true,
-			filter_saveFilters: true,
-			filter_reset: "button.reset",
-			filter_functions: {
-				".filter-version": true,
-				".filter-downloaded": true,
-				".filter-installed": true
-			}
-		}
-	});
-
 	// "uninstall package" switch and cookie
 	$("#uninstallpkg")
 		.switchButton({
@@ -39,25 +21,6 @@ $(function(){
 		})
 		.change(function () {
 			$.cookie("nerdpack_packages_delete", $(this).prop("checked") ? "--delete" : "", { expires: 3650 });
-		});
-
-	// select all packages switch
-	$("#checkall")
-		.switchButton({
-			labels_placement: "right",
-			on_label: "Select All",
-			off_label: "Select All",
-			checked: $.cookie("nerdpack_checkall") == "yes"
-		})
-		.change(function () {
-			var myval = $(this).prop("checked");
-			$.cookie("nerdpack_checkall", myval ? "yes" : "no", { expires: 3650 });
-			$(".pkgcheckbox").each(function() { // loop through each checkbox
-				$(this).switchButton({checked: myval});
-			});
-			$(".pkgvalue").each(function() { // loop through each value
-				$(this).val(myval ? "yes": "no");
-			});
 		});
 
 	// set cookie on apply button press
@@ -126,6 +89,43 @@ function packageQuery(force) {
 				});
 		}
 		$("#tblPackages").trigger("update");
+		
+		// tablesorter options
+		$("#tblPackages").tablesorter({
+			sortList: [[0,0]],
+			widgets: ["saveSort", "filter", "stickyHeaders"],
+			widgetOptions: {
+				stickyHeaders_filteredToTop: true,
+				filter_hideEmpty: true,
+				filter_liveSearch: true,
+				filter_saveFilters: true,
+				filter_reset: ".reset",
+				filter_functions: {
+					".filter-version": true,
+					".filter-downloaded": true,
+					".filter-installed": true
+				}
+			}
+		});
+
+		// select all packages switch
+		$("#checkall")
+			.switchButton({
+				labels_placement: "right",
+				on_label: "Select All",
+				off_label: "Select All",
+				checked: $.cookie("nerdpack_checkall") == "yes"
+			})
+			.change(function () {
+				var myval = $(this).prop("checked");
+				$.cookie("nerdpack_checkall", myval ? "yes" : "no", { expires: 3650 });
+				$(".pkgcheckbox").each(function() { // loop through each checkbox
+					$(this).switchButton({checked: myval});
+				});
+				$(".pkgvalue").each(function() { // loop through each value
+					$(this).val(myval ? "yes": "no");
+				});
+			});
 	});
 }
 
