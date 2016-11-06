16846d0db8b83d596ed393cfe676219ec5d68aa3
dmacias72 <derekmacias@live.com>
2016-11-06 11:17:39 -0700

    e Please enter the commit message for your changes. Lines starting remove node modules :wq

--- source/ungit/usr/local/emhttp/plugins/ungit/node_modules/ungit/source/utils/file-type.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const path = require('path');
-const imageFileExtensions = ['.PNG', '.JPG', '.BMP', '.GIF', '.JPEG'];
-
-module.exports = (fileName) => imageFileExtensions.indexOf(path.extname(fileName).toUpperCase()) > -1 ? 'image' : 'text';
--- source/ungit/usr/local/emhttp/plugins/ungit/node_modules/ungit/src/utils/file-type.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var path = require('path');
-var imageFileExtensions = ['.PNG', '.JPG', '.BMP', '.GIF', '.JPEG'];
-
-module.exports = function (fileName) {
-  return imageFileExtensions.indexOf(path.extname(fileName).toUpperCase()) > -1 ? 'image' : 'text';
-};
