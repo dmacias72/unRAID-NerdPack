0494a980f3669004a49cb63d31fff372d74a31d4
dmacias72 <derekmacias@live.com>
2016-11-06 14:27:43 -0700

    fix port not saving

--- source/ungit/usr/local/emhttp/plugins/ungit/include/settings.php
+++ source/ungit/usr/local/emhttp/plugins/ungit/include/settings.php
@@ -7,8 +7,8 @@ $gdaemon = isset($gcfg['DAEMON']) ? $gcfg['DAEMON'] : "disable";
 //$qipaddr = preg_match('/^(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:[.](?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}$/', $ipmi_cfg['host']) ?
 //    $qcfg['host'] : $var['IPADDR'];
 $gipaddr = 'localhost';
-$gport   = (isset($gcfg['port']) && is_numeric($gcfg['port']) && $gcfg['port'] > 0 && $gcfg['port'] < 65535 ) ? $gcfg['port'] : 8448;
-$gversion = shell_exec( '/usr/local/emhttp/plugins/ungit/node_modules/ungit/bin/ungit -v' );
+$gport   = (isset($gcfg['PORT']) && is_numeric($gcfg['PORT']) && $gcfg['PORT'] > 0 && $gcfg['PORT'] < 65535 ) ? $gcfg['PORT'] : 8448;
+$gversion = trim(shell_exec( 'ungit -v' ));
 
 //check running status
 $grunning = (trim(shell_exec( "[ -f /proc/`cat /var/run/ungit.pid 2> /dev/null`/exe ] && echo 1 || echo 0 2> /dev/null" )) == 1);
